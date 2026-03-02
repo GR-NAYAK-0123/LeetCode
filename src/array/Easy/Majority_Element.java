191904e56285e77ed7643797917371ad8387700a
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-02 17:41:18 +0530

    Solved the Majority element problem with optimal approach (More's Voting Algorithm)

--- src/array/Easy/Majority_Element.java
+++ src/array/Easy/Majority_Element.java
@@ -26,4 +26,24 @@ public class Majority_Element {
         }
         return 0;
     }
+
+    // More's Voting Algorithm
+    // Optimal Solution having time complexity - O(n) and space complexity is O(1)
+    public int majorityElement1(int[] nums) {
+        int element = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(count == 0){
+                element = nums[i];
+                count++;
+            }
+            else if(element == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return element;
+    }
 }

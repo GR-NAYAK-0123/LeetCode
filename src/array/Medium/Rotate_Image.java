05dcdd8d12b60cf91e49e301e9dbe273dba48a80
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-04 01:31:43 +0530

    Used an Optima approach for the solving the Rotate Image problem

--- src/array/Medium/Rotate_Image.java
+++ src/array/Medium/Rotate_Image.java
@@ -21,4 +21,26 @@ public class Rotate_Image {
         }
 
     }
+    // Optima Approach with time complexity - O(n/2 * n/2) + O(n + n/2) and constant space
+    public void rotate1(int[][] matrix) {
+        // Optimal Approach
+        // Step - 1 -> Transpose the matrix
+        int length = matrix.length;
+        for(int i = 1;i<matrix.length;i++){
+            for(int j = 0;j<=i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //Step - 2 -> Reverse each row
+        for(int i = 0;i<matrix.length;i++){
+            for(int j = 0;j<matrix.length / 2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length-j-1];
+                matrix[i][length-j-1] = temp;
+            }
+        }
+
+    }
 }

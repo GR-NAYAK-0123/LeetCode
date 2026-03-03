217c434d09a4a494c2d56aece3e59a5c12e2c4e0
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-03 16:09:58 +0530

    Solved the problem by using optimal approach

--- src/array/Medium/Longest_Consecutive_Sequence.java
+++ src/array/Medium/Longest_Consecutive_Sequence.java
@@ -5,6 +5,7 @@
 package array.Medium;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Longest_Consecutive_Sequence {
     // Normal Approach
@@ -25,6 +26,31 @@ public class Longest_Consecutive_Sequence {
             }
             else {
                 count = 1;
+            }
+        }
+        return maxLength;
+    }
+    // Optimal Approach by using Set with time complexity - O(n) + O(n) and space complexity - O(n)
+    public int longestConsecutive1(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int maxLength = 1;
+        int count = 0;
+        int start = Integer.MIN_VALUE;
+        for(int value : set){
+            if(!set.contains(value - 1)){
+                count = 1;
+                start = value;
+                while(set.contains(start+1)){
+                    count++;
+                    start++;
+                }
+                maxLength = Math.max(maxLength,count);
             }
         }
         return maxLength;

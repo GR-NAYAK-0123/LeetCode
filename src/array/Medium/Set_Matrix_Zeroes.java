e7abfed6b0e1da69334f5ca16afcfc2adae3c0cb
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-03 23:53:13 +0530

    Just modified the array declaration

--- src/array/Medium/Set_Matrix_Zeroes.java
+++ src/array/Medium/Set_Matrix_Zeroes.java
@@ -6,8 +6,8 @@ package array.Medium;
 
 public class Set_Matrix_Zeroes {
     public void setZeroes(int[][] matrix) {
-        int row[] = new int[matrix.length];
-        int column[] = new int[matrix[0].length];
+        int[] row = new int[matrix.length];
+        int[] column = new int[matrix[0].length];
         for(int i = 0;i<matrix.length;i++){
             for(int j = 0;j<matrix[i].length;j++){
                 if(matrix[i][j] == 0){

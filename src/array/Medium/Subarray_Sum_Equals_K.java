704332eb1b33c8246e4b5dde58841cb49eec55d5
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-06 02:39:01 +0530

    Solved the problem - Subarray Sum Equals K with Optimal Approach

--- src/array/Medium/Subarray_Sum_Equals_K.java
+++ src/array/Medium/Subarray_Sum_Equals_K.java
@@ -5,6 +5,8 @@
 
 package array.Medium;
 
+import java.util.HashMap;
+
 public class Subarray_Sum_Equals_K {
     // Brute force Approach
     public int subarraySum(int[] nums, int k) {
@@ -16,6 +18,28 @@ public class Subarray_Sum_Equals_K {
                 if(sum == k){
                     count++;
                 }
+            }
+        }
+        return count;
+    }
+
+    // Optimal Approach
+    public int subarraySum1(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int preSum = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            preSum += nums[i];
+            if(map.containsKey(preSum-k)){
+                count += map.get(preSum-k);
+            }
+
+            if(map.containsKey(preSum)){
+                map.put(preSum, map.get(preSum)+1);
+            }
+            else{
+                map.put(preSum, 1);
             }
         }
         return count;

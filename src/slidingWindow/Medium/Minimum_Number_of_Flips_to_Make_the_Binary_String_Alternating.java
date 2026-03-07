ee41f9a641313885692c8d52608abb827b5bd911
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-07 11:18:25 +0530

    Solved the problem - Minimum Number of Flips to Make the Binary String Alternating

---
+++ src/slidingWindow/Medium/Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating.java
@@ -0,0 +1,58 @@
+// Problem: Minimum Number of Flips to Make the Binary String Alternating
+// Link: https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/
+// Time: O(n)
+// Space: O(1)
+
+package slidingWindow.Medium;
+
+public class Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating {
+    public int minFlips(String s) {
+        int length = s.length();
+        StringBuilder s1 = new StringBuilder("");
+        StringBuilder s2 = new StringBuilder("");
+        for(int i = 0;i<2 * s.length();i++){
+            if(i % 2 == 0){
+                s1.append('0');
+                s2.append('1');
+            }
+            else{
+                s1.append('1');
+                s2.append('0');
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        int flip1 = 0;
+        int flip2 = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while(j < 2*length){
+
+            if(s.charAt(j % length) != s1.charAt(j)){
+                flip1++;
+            }
+            if(s.charAt(j % length) != s2.charAt(j)){
+                flip2++;
+            }
+
+            if(j-i+1 > length){ // For shrink the window from left
+
+                if(s.charAt(i % length) != s1.charAt(i)){
+                    flip1--;
+                }
+                if(s.charAt(i % length) != s2.charAt(i)){
+                    flip2--;
+                }
+                i++;
+            }
+
+            if(j-i+1 == length){
+                result = Math.min(result, flip1);
+                result = Math.min(result, flip2);
+            }
+            j++;
+        }
+        return result;
+    }
+}

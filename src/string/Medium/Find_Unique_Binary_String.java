fd1f06d9a1250bd709075758f57fd4b4a612a310
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-08 17:58:37 +0530

    Used another approach to solve this problem

--- src/string/Medium/Find_Unique_Binary_String.java
+++ src/string/Medium/Find_Unique_Binary_String.java
@@ -5,6 +5,8 @@
 
 package string.Medium;
 
+import java.util.HashSet;
+
 public class Find_Unique_Binary_String {
     //Best Approach
     public String findDifferentBinaryString(String[] nums) {
@@ -20,5 +22,21 @@ public class Find_Unique_Binary_String {
             }
         }
         return result.toString();
+    }
+    // Normal Approach
+    public String findDifferentBinaryString1(String[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(String binary : nums){
+            Integer decimal = Integer.parseInt(binary, 2);
+            set.add(decimal);
+        }
+        for(int i = 0;i<=n;i++){
+            if(!set.contains(i)){
+                String binary = String.format("%16s", Integer.toBinaryString(i)).replace(' ', '0');
+                return binary.substring(16-n);
+            }
+        }
+        return "";
     }
 }

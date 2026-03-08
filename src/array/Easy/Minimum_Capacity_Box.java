2fdf9cfb529a090eab429b6f725d514a35fd4bf6
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-09 03:49:09 +0530

    Solved the problem - Minimum Capacity box

---
+++ src/array/Easy/Minimum_Capacity_Box.java
@@ -0,0 +1,23 @@
+// Problem: Minimum Capacity Box
+// Link: https://leetcode.com/problems/minimum-capacity-box/description/
+// Time: O(n)
+// Space: O(1)
+
+package array.Easy;
+
+public class Minimum_Capacity_Box {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int n = capacity.length;
+        int smallestIndex = -1;
+        int minimum = Integer.MAX_VALUE;
+        for(int i = n-1;i>=0;i--){
+            if(capacity[i] >= itemSize){
+                if(capacity[i] <= minimum){
+                    minimum = capacity[i];
+                    smallestIndex = i;
+                }
+            }
+        }
+        return smallestIndex;
+    }
+}

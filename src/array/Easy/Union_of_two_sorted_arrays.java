49376072f71299490ddb807c178efb3ba7830085
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-13 02:58:25 +0530

    Just changed the array declaration and also added one comment

--- src/array/Easy/Union_of_two_sorted_arrays.java
+++ src/array/Easy/Union_of_two_sorted_arrays.java
@@ -7,6 +7,7 @@ package array.Easy;
 import java.util.HashSet;
 
 public class Union_of_two_sorted_arrays {
+    // Basic Approach for solving the problem
     public int[] unionArray(int[] nums1, int[] nums2) {
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0;i<nums1.length;i++){
@@ -15,7 +16,7 @@ public class Union_of_two_sorted_arrays {
         for(int i = 0;i<nums2.length;i++){
             set.add(nums2[i]);
         }
-        int result[] = new int[set.size()];
+        int[] result = new int[set.size()];
         int j = 0;
         for(Integer value : set){
             result[j] = value;

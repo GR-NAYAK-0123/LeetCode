4349cbd1456ec701d6fc1954b4a8d1d34d552381
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-23 00:27:25 +0530

    Solved the problem - Palindrome Number

---
+++ src/math/Easy/Palindrome_Number.java
@@ -0,0 +1,21 @@
+// Problem: Palindrome Number
+// Link: https://leetcode.com/problems/palindrome-number/description/
+// Time: O(n)
+// Space: O(1)
+
+package math.Easy;
+
+public class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int value = x;
+        int newValue = 0;
+        while(value != 0){
+            int last = value % 10;
+            newValue = last + (newValue * 10);
+            value /= 10;
+        }
+        if(newValue == x) return true;
+        return false;
+    }
+}

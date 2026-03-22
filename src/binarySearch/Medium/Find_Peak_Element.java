ef8cdd27c6da3c841f47cf06abca36d66f4cd8d1
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-22 18:57:28 +0530

    Solved the problem - Find peak Element by using Linear Search approach (Basic Approach)

---
+++ src/binarySearch/Medium/Find_Peak_Element.java
@@ -0,0 +1,21 @@
+// Problem: Find Peak Element
+// Link: https://leetcode.com/problems/find-peak-element/description/
+// Time: O(log(n))
+// Space: O(1)
+
+package binarySearch.Medium;
+
+public class Find_Peak_Element {
+    // This is very Basic Approach by using Linear Search of time complexity O(n)
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        for(int i = 1;i<n-1;i++){
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+        }
+        return -1;
+    }
+}

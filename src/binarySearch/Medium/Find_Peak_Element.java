0354bb5680d05646868deecb5c749ca9269e6aa0
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-22 21:42:06 +0530

    Solved the problem - Find peak Element by using Binary Search approach (Best Approach)

--- src/binarySearch/Medium/Find_Peak_Element.java
+++ src/binarySearch/Medium/Find_Peak_Element.java
@@ -6,6 +6,29 @@
 package binarySearch.Medium;
 
 public class Find_Peak_Element {
+    // Best Binary Search Approach
+    public int findPeakElement1(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        int start = 1;
+        int end = n-2;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] > nums[mid+1]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
     // This is very Basic Approach by using Linear Search of time complexity O(n)
     public int findPeakElement(int[] nums) {
         int n = nums.length;

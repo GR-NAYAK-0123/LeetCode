1e724349d56baf19346ed9753c1dd2ac13dc1fe4
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-22 18:42:14 +0530

    Solved the problem - Single Element in a Sorted Array by using Binary Search (Best Approach)

--- src/binarySearch/Medium/Single_Element_in_a_Sorted_Array.java
+++ src/binarySearch/Medium/Single_Element_in_a_Sorted_Array.java
@@ -6,6 +6,30 @@
 package binarySearch.Medium;
 
 public class Single_Element_in_a_Sorted_Array {
+    public int singleNonDuplicate1(int[] nums) {
+        //Best Approach
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int start = 1;
+        int end = n-2;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            if((mid % 2 != 0 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])) {
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     // Pure Brute force with time complexity O(n) (Basic Approach)
     public int singleNonDuplicate(int[] nums) {
         if(nums.length == 1) return nums[0];

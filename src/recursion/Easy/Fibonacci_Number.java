c8ecbdcccdb88d6b72e46dbbef982b5612c000ff
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-24 22:47:15 +0530

    Added the detailed time complexity

--- src/recursion/Easy/Fibonacci_Number.java
+++ src/recursion/Easy/Fibonacci_Number.java
@@ -1,6 +1,6 @@
 // Problem: Fibonacci Number
 // Link: https://leetcode.com/problems/fibonacci-number/description/
-// Time: O(2*n)
+// Time: O(2*n) (Exponential Time Complexity but not exactly 2*n but bit less)
 // Space: O(1)
 
 package recursion.Easy;

5598ca954cf5718a55fab4d443b24fec6b902d21
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-25 10:25:02 +0530

    Added the Bubble Sort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,18 @@ public class Main {
            arr[minIdx] = temp;
         }
     }
+    public static void bubble(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("LeetCode");
 
---
+++ src/sorting/Easy/Bubble_Sort.java
@@ -0,0 +1,21 @@
+// Sorting: Bubble Sort
+// Time: O(n*2)
+// Space: O(1)
+
+package sorting.Easy;
+
+public class Bubble_Sort {
+    // Here we just have to swap with near value if the current value is greater that next value
+    public void bubble(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}

5cb9ef026d6fcc4e2d69ce7788ca2dea75d113ac
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-03-31 09:31:56 +0530

    Solved the problem - Median of Two Sorted Arrays

---
+++ src/binarySearch/Hard/Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,30 @@
+// Problem: Median of Two Sorted Arrays
+// Link: https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+// Time: O(n)
+// Space: O(n)
+
+package binarySearch.Hard;
+
+public class Median_of_Two_Sorted_Arrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] result = new int[n+m];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < n && j < m){
+            if(nums1[i] <= nums2[j]) result[k++] = nums1[i++];
+            else result[k++] = nums2[j++];
+        }
+        while(i < n){
+            result[k++] = nums1[i++];
+        }
+        while(j < m){
+            result[k++] = nums2[j++];
+        }
+        if(result.length % 2 != 0) return (double)result[result.length / 2];
+        return (double)(result[result.length / 2] + result[(result.length / 2) - 1]) / 2;
+    }
+}

750fc62bd3a3d1890a1e8c7bad69cff3eb6ee71c
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-07 10:58:25 +0530

    Solved the problem - Reverse Words in a String, by using another approach

--- src/string/Medium/Reverse_Words_in_a_String.java
+++ src/string/Medium/Reverse_Words_in_a_String.java
@@ -8,7 +8,7 @@ package string.Medium;
 public class Reverse_Words_in_a_String {
     //Not an efficient approach
     public String reverseWords(String s) {
-        //First i need to remove if there is any leading or trailing space
+        //First I need to remove if there is any leading or trailing space
         String s1 = s.trim();
         StringBuilder result = new StringBuilder();
         String temp = "";
@@ -39,5 +39,41 @@ public class Reverse_Words_in_a_String {
             result.append(temp);
         }
         return result.toString().trim();
+    }
+
+    //This is another approach, Without having any trim() method
+    public String reverseWords1(String s) {
+        StringBuilder result = new StringBuilder();
+        String temp = "";
+        int count = 0;
+        for(int i = s.length() -1;i>=0;i--){
+            char current = s.charAt(i);
+
+            if(current != ' '){
+                temp = current + temp;
+            }
+            else{
+                if(temp == "" && result.length() == 0) continue;
+                else {
+                    if(result.length() != 0 && temp.length() > 0){
+                        result.append(' ');
+                        result.append(temp);
+                        temp = "";
+                    }
+                    else{
+                        result.append(temp);
+                        temp = "";
+                    }
+                }
+            }
+        }
+        if(temp.length() >= 1 && result.length() == 0){
+            result.append(temp);
+        }
+        else if(temp.length() >= 1) {
+            result.append(' ');
+            result.append(temp);
+        }
+        return result.toString().substring(0,result.length());
     }
 }

208e1c9a726c5f79cb741c574feb152d1d3559ed
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-10 12:48:58 +0530

    I have used another approach to solve the problem - Longest Palindromic Substring

--- src/string/Medium/Longest_Palindromic_Substring.java
+++ src/string/Medium/Longest_Palindromic_Substring.java
@@ -28,4 +28,34 @@ public class Longest_Palindromic_Substring {
         }
         return s.substring(start, start + maxLength);
     }
+
+    // This is same approach but here I used iterative approach to check the palindrome
+    // Using this iterative approach I reduced the space time complexity O(n) to O(1)
+    public boolean checkPalindrome1(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    public String longestPalindrome1(String s) {
+        int n = s.length();
+        int maxLength = -1;
+        int start = -1;
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                if(checkPalindrome(s, i, j)){
+                    int currentLength = (j-i) + 1;
+                    if(currentLength > maxLength){
+                        maxLength = currentLength;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLength);
+    }
 }

4dcf1cccc250131aaaf2f7318e7c2ac0e6376f5c
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-11 11:03:34 +0530

    Here I have added another better approach to solve the problem - Sum of Beauty of All Substrings

--- src/string/Medium/Sum_of_Beauty_of_All_Substrings.java
+++ src/string/Medium/Sum_of_Beauty_of_All_Substrings.java
@@ -44,4 +44,31 @@ public class Sum_of_Beauty_of_All_Substrings {
         }
         return result;
     }
+
+    // Here is the another way to solve this problem to reduce the time and space complexity little bit
+    // Here the time complexity will be O(n * 2) and space complexity is O(1)
+    public int getBeautyValue(int[] freq){
+        int max = freq[0];
+        int min = freq[0];
+        for(int k = 1;k<freq.length;k++){
+            if(freq[k] > max){
+                max = freq[k];
+                if(min == 0) min = freq[k];
+            }
+            else if(freq[k] != 0 && freq[k] < min) min = freq[k];
+        }
+        return max - min;
+    }
+    public int beautySum1(String s) {
+        int n = s.length();
+        int result = 0;
+        for(int i = 0;i<n;i++){
+            int[] freq = new int[26];
+            for(int j = i;j<n;j++){
+                freq[s.charAt(j) - 'a']++;
+                result += getBeautyValue(freq);
+            }
+        }
+        return result;
+    }
 }

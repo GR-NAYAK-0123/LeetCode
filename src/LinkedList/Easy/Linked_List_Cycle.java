7562a3b1b734e06841f39d7261947068ea821b0d
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-21 12:29:35 +0530

    I have added another Hash Map approach to solve the problem - Linked List Cycle

--- src/LinkedList/Easy/Linked_List_Cycle.java
+++ src/LinkedList/Easy/Linked_List_Cycle.java
@@ -5,6 +5,8 @@
 
 package LinkedList.Easy;
 
+import java.util.HashMap;
+
 public class Linked_List_Cycle {
     // Best approach to solve this problem
     public boolean hasCycle(ListNode head) {
@@ -15,6 +17,20 @@ public class Linked_List_Cycle {
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast) return true;
+        }
+        return false;
+    }
+
+    /* Here is another approach to solve this problem by using HashMap, but it takes O(n) time and O(n) space
+       for the HashMap
+     */
+    public boolean hasCycle1(ListNode head) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        ListNode temp = head;
+        while(temp != null){
+            if(map.containsKey(temp)) return true;
+            map.put(temp, 1);
+            temp = temp.next;
         }
         return false;
     }

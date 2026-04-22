e7590fdb47e817ecc2ee0180c40caa6db03fcc05
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-22 12:26:16 +0530

    Solved the problem - Linked List Cycle II, by using HashMap

---
+++ src/LinkedList/Medium/Linked_List_Cycle_II.java
@@ -0,0 +1,22 @@
+// Problem: Linked List Cycle II
+// Link: https://leetcode.com/problems/linked-list-cycle-ii/description/
+// Time: O(n)
+// Space: O(n) Here is the extra space is for the HashMap
+
+package LinkedList.Medium;
+
+import java.util.HashMap;
+
+public class Linked_List_Cycle_II {
+    // Basic Brute force approach to solve this problem
+    public ListNode detectCycle(ListNode head) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        ListNode temp = head;
+        while(temp != null){
+            if(map.containsKey(temp)) return temp;
+            map.put(temp, 1);
+            temp = temp.next;
+        }
+        return null;
+    }
+}

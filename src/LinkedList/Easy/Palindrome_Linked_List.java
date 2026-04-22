40a52cf1ed0ab316976042d726505c7a6aecbcb2
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-22 14:18:04 +0530

    Solved the problem - Palindrome Linked List, by using best approach which is without using any space

--- src/LinkedList/Easy/Palindrome_Linked_List.java
+++ src/LinkedList/Easy/Palindrome_Linked_List.java
@@ -24,4 +24,38 @@ public class Palindrome_Linked_List {
         }
         return true;
     }
+
+    // Here is another better approach to solve this problem without taking any extra space
+    // Here the time complexity will be [O(n) + O(n/2) + O(n/2)] and O(1) space complexity
+    // O(n) for finding the middle of the linked list
+    // O(n/2) for reversing the half of the linked list
+    // O(n/2) for traversing both the linked list
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode front = head;
+        while(head != null){
+            front = front.next;
+            head.next = prev;
+            prev = head;
+            head = front;
+        }
+        return prev;
+    }
+    public boolean isPalindrome1(ListNode head) {
+        // First we have to find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = reverse(slow);
+        fast = head;
+        while(newHead != null && fast != null){
+            if(newHead.val != fast.val) return false;
+            newHead = newHead.next;
+            fast = fast.next;
+        }
+        return true;
+    }
 }

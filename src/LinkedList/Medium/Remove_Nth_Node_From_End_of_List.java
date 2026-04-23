19eccc06a0e5bac003f8193d01e839d3fb81033b
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-23 13:29:30 +0530

    Solved the problem - Remove Nth Node From End of List, with best approach

--- src/LinkedList/Medium/Remove_Nth_Node_From_End_of_List.java
+++ src/LinkedList/Medium/Remove_Nth_Node_From_End_of_List.java
@@ -29,4 +29,20 @@ public class Remove_Nth_Node_From_End_of_List {
         prev.next = temp.next;
         return head;
     }
+    // Another better approach to solve this problem with O(n) time only [In one iteration]
+    public ListNode removeNthFromEnd_1(ListNode head, int n) {
+        ListNode front = head;
+        while(n != 0){
+            front = front.next;
+            n--;
+        }
+        if(front == null) return head.next;
+        ListNode back = head;
+        while(front.next != null){
+            back = back.next;
+            front = front.next;
+        }
+        back.next = back.next.next;
+        return head;
+    }
 }

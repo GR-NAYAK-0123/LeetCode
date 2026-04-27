036f3ce69e1a7ef6535fc1d0f6a1ca20aeda698c
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-27 10:43:38 +0530

    Solved the problem - Intersection of Two Linked Lists, and Added proper time and space complexity

--- src/LinkedList/Easy/Intersection_of_Two_Linked_Lists.java
+++ src/LinkedList/Easy/Intersection_of_Two_Linked_Lists.java
@@ -1,6 +1,6 @@
 // Problem: Intersection of Two Linked Lists
 // Link: https://leetcode.com/problems/intersection-of-two-linked-lists/description/
-// Time: O(n) + O(m) + O(n)
+// Time: O(n) + O(m) + O(diff) + O(n or m)
 // Space: O(1)
 
 package LinkedList.Easy;
@@ -9,49 +9,35 @@ import java.util.HashMap;
 
 public class Intersection_of_Two_Linked_Lists {
     // Basic approach to solve this problem
+    public ListNode meeting_point(ListNode small, ListNode big, int diff){
+        ListNode temp = big;
+        while(diff != 0){
+            diff--;
+            temp = temp.next;
+        }
+        ListNode temp1 = small;
+        while(temp != temp1){
+            temp = temp.next;
+            temp1 = temp1.next;
+        }
+        return temp;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int len_a = 0;
-        int len_b = 0;
+        int n = 0;
+        int m = 0;
         ListNode temp = headA;
         while(temp != null){
-            len_a++;
+            n++;
             temp = temp.next;
         }
         temp = headB;
         while(temp != null){
-            len_b++;
+            m++;
             temp = temp.next;
         }
-        int diff = 0;
-        if(len_a > len_b){
-            diff = len_a - len_b;
-            temp = headA;
-            while(diff != 0){
-                diff--;
-                temp = temp.next;
-            }
-            ListNode temp1 = headB;
-            while(temp1 != null){
-                if(temp1 == temp) return temp1;
-                temp = temp.next;
-                temp1 = temp1.next;
-            }
-        }
-        else{
-            diff = len_b - len_a;
-            temp = headB;
-            while(diff != 0){
-                diff--;
-                temp = temp.next;
-            }
-            ListNode temp1 = headA;
-            while(temp1 != null){
-                if(temp1 == temp) return temp1;
-                temp = temp.next;
-                temp1 = temp1.next;
-            }
-        }
-        return null;
+
+        if(n > m) return meeting_point(headB, headA, n - m);
+        else return meeting_point(headA, headB, m - n);
     }
     /*
        This is brute force approach by using HashMap and it took O(n) + O(m) time and O(n) space
